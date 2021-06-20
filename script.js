754729828b833711d2ef6866c54c4d3c1e364072
Nuraym Omurbekova <omurbekovanuraym@gmail.com>
2021-06-20 17:54:33 +0600

    added inractive for gallery

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector(".modal");
+    const modal = document.querySelector(".modal-overlay");
 		const openBtn = document.querySelector(".open-modal");
 		const closeBtn = document.querySelector(".close-btn");
 
@@ -18,4 +18,37 @@ const modal = document.querySelector(".modal");
 		window.addEventListener("click", windowOnClick);
 		openBtn.addEventListener("click", toggleModal)
 		closeBtn.addEventListener("click", toggleModal)
+
+    const form = document.querySelector(".form");
+    const name = document.querySelector(".name")
+
+    let formObj = {};
+
+    form.onsubmit = function() {
+      var name = this.name.value;
+      var email = this.email.value;
+      var text = this.text.value;
+      formObj.name = name;
+      formObj.email = email;
+      formObj.text = text;
+      console.log(formObj);
+      return false;
+    };
+
+		const modalOverlay = document.querySelector(".modal-overlay2");
+    function toggleModal2 () {
+			modalOverlay.classList.toggle("show-modal2");
+		}
+
+		function windowOnClick2(event) {
+			if(event.target === modalOverlay){
+				toggleModal2();
+			}
+		}
+    const image = document.querySelector(".card-image")
+    const closeImgButton = document.querySelector(".close-image_btn")
+
+    window.addEventListener("click", windowOnClick2);
+		image.addEventListener("click", toggleModal2);
+		closeImgButton.addEventListener("click", toggleModal2);
 		
